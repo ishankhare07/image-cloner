1349800656f99855dd218026bac75876a0ffd174
Ishan Khare <ishankhare07@gmail.com>
2021-03-20 17:03:16 +0530

    add image exists check function
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- pkg/registry/registry.go
+++ pkg/registry/registry.go
@@ -5,27 +5,28 @@ import (
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
 )
 
 type Interface interface {
 	GetImage(imgName string) (v1.Image, error)
+	ImageExists(imageName string) (bool, name.Reference, error)
 	Upload(img v1.Image, imageName string) error
 }
 
-type RegistryOption func(*registry)
+type RegistryClientOption func(*registryClient)
 
-type registry struct {
-	registry      string
-	Reference     name.Reference
-	Authenticator authn.Authenticator
+type registryClient struct {
+	registryName  string
+	authenticator authn.Authenticator
 }
 
-func (r *registry) GetImage(imgName string) (v1.Image, error) {
+func (r *registryClient) GetImage(imgName string) (v1.Image, error) {
 	var registryName string
-	if r.registry == "" {
+	if r.registryName == "" {
 		registryName = imgName
 	} else {
-		registryName = r.registry + "/" + imgName
+		registryName = r.registryName + "/" + imgName
 	}
 
 	ref, err := name.ParseReference(registryName)
@@ -33,41 +34,64 @@ func (r *registry) GetImage(imgName string) (v1.Image, error) {
 		return nil, err
 	}
 
-	return remote.Image(ref, remote.WithAuth(r.Authenticator))
+	return remote.Image(ref, remote.WithAuth(r.authenticator))
 }
 
-func (r *registry) Upload(img v1.Image, imageName string) error {
-	ref, err := name.ParseReference(r.registry + "/" + imageName)
+func (r *registryClient) Upload(img v1.Image, imageName string) error {
+	exists, ref, err := r.ImageExists(imageName)
+	if exists {
+		return nil
+	}
+
 	if err != nil {
 		return err
 	}
 
-	return remote.Write(ref, img, remote.WithAuth(r.Authenticator))
+	return remote.Write(ref, img, remote.WithAuth(r.authenticator))
 }
 
-func WithName(registryName string) RegistryOption {
-	return func(r *registry) {
-		r.registry = registryName
+func (r *registryClient) ImageExists(imageName string) (bool, name.Reference, error) {
+	ref, err := name.ParseReference(r.registryName + "/" + imageName)
+	if err != nil {
+		return false, nil, err
+	}
+
+	_, err = remote.Image(ref, remote.WithAuth(r.authenticator))
+	if err != nil {
+		terr, _ := err.(*transport.Error)
+		for _, ec := range terr.Errors {
+			if ec.Code == transport.ManifestUnknownErrorCode {
+				return false, nil, nil
+			}
+		}
+	}
+
+	return true, ref, nil
+}
+
+func WithName(registryName string) RegistryClientOption {
+	return func(r *registryClient) {
+		r.registryName = registryName
 	}
 }
 
-func WithAuth(cfg authn.AuthConfig) RegistryOption {
-	return func(r *registry) {
-		r.Authenticator = authn.FromConfig(cfg)
+func WithAuth(cfg authn.AuthConfig) RegistryClientOption {
+	return func(r *registryClient) {
+		r.authenticator = authn.FromConfig(cfg)
 	}
 }
 
-func NewRegistry(registryOptions ...RegistryOption) Interface {
+func NewRegistryClient(registryClientOptions ...RegistryClientOption) Interface {
 	const (
 		defaultName = ""
 	)
 
-	r := &registry{
-		registry:      defaultName,
-		Authenticator: authn.Anonymous,
+	r := &registryClient{
+		registryName:  defaultName,
+		authenticator: authn.Anonymous,
 	}
 
-	for _, opt := range registryOptions {
+	for _, opt := range registryClientOptions {
 		opt(r)
 	}
 
